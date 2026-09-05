1f4f72577a127984c42239f9b512c55b7a40c8a6
francesco-guttuso <francescoguttuso@gmail.com>
2026-09-05 09:08:25 +0200

    Add posts router with CRUD operations

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import posts from "./data/posts.js";
+import postsRouter from "./routers/posts.js";
+
+const app = express();
+const port = 3000;
+
+app.use("/posts", postsRouter);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
+});
+
+app.get("/bacheca", (req, res) => {
+  res.json(posts);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ routers/posts.js
@@ -0,0 +1,27 @@
+import express from "express";
+import posts from "../data/posts.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json(posts);
+});
+
+router.get("/:id", (req, res) => {
+  const post = posts.find((post) => post.id === Number(req.params.id));
+  res.json(post);
+});
+
+router.post("/", (req, res) => {
+  res.send("Creazione di un nuovo post");
+});
+
+router.put("/:id", (req, res) => {
+  res.send(`Modifica del post ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+  res.send(`Cancellazione del post ${req.params.id}`);
+});
+
+export default router;
